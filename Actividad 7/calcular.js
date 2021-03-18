512e023aae177d361dee3d309382496523e23604
Alan Varela <a271455@uach.mx>
2021-03-17 21:31:27 -0700

    Added activity 7

---
+++ Actividad 7/calcular.js
@@ -0,0 +1,20 @@
+function calculate(numero1, numero2, value){
+
+    var result;
+
+    if(value == 1){
+        result = parseFloat(numero1) + parseFloat(numero2);
+        document.getElementById("resultado").innerHTML = result;
+    } else if (value == 2){
+        result = parseFloat(numero1) - parseFloat(numero2);
+        document.getElementById("resultado").innerHTML = result;
+    } else if(value == 3){
+        result = parseFloat(numero1) / parseFloat(numero2);
+        document.getElementById("resultado").innerHTML = result;
+    } else if (value == 4){
+        result = parseFloat(numero1) * parseFloat(numero2);
+        document.getElementById("resultado").innerHTML = result;
+    } else {
+        alert("Error unkown operation");
+    }
+}
