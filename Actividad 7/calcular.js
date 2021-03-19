a1a4177134f67ac61f9faf58192edded0076ff42
Alan Varela <a271455@uach.mx>
2021-03-18 17:46:01 -0700

    Finished Actividad 7

--- Actividad 7/calcular.js
+++ Actividad 7/calcular.js
@@ -9,10 +9,10 @@ function calculate(numero1, numero2, value){
         result = parseFloat(numero1) - parseFloat(numero2);
         document.getElementById("resultado").innerHTML = result;
     } else if(value == 3){
-        result = parseFloat(numero1) / parseFloat(numero2);
+        result = parseFloat(numero1) * parseFloat(numero2);
         document.getElementById("resultado").innerHTML = result;
     } else if (value == 4){
-        result = parseFloat(numero1) * parseFloat(numero2);
+        result = parseFloat(numero1) / parseFloat(numero2);
         document.getElementById("resultado").innerHTML = result;
     } else {
         alert("Error unkown operation");
