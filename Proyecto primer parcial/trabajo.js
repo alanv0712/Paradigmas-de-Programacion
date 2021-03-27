b7055a0dd0a2b0e02bf892515d4d3e75b771f051
Alan Varela <a271455@uach.mx>
2021-03-26 23:51:59 -0700

    Proyecto primer parcial

---
+++ Proyecto primer parcial/trabajo.js
@@ -0,0 +1,150 @@
+const  changeImage1 = () => {
+    var startTime1 = new Date();
+    var endTime1;
+    var total1;
+    var difference1;
+    var timer1;
+    element = document.getElementById('myImage1')
+    if (element.src.match("On")){
+        endTime1 = new Date();
+        timer1 = endTime1.getHours() + ":" + endTime1.getMinutes() + ":" + endTime1.getSeconds();
+        difference1 = Math.round(endTime1 - startTime1);
+        total1 = Math.round((difference1 / 60000) / 60);
+        total1 = total1 * 52;
+        element.src = "/Proyecto primer parcial/images/Off.png"
+        document.getElementById('fin1').innerHTML = "Hora del fin: " + timer1;
+        document.getElementById('total1').innerHTML = "Total: " + total1;
+    } else {
+        startTime1 = new Date();
+        document.getElementById('fin1').innerHTML = "Hora del fin: ";
+        document.getElementById('total1').innerHTML = "Total: ";
+        timer1 = startTime1.getHours() + ":" + startTime1.getMinutes() + ":" + startTime1.getSeconds();
+        element.src = "/Proyecto primer parcial/images/On.png"
+        document.getElementById('inicio1').innerHTML = "Hora de Inicio: " + timer1;
+    }   
+}
+const  changeImage2 = () => {
+    var startTime2 = new Date();
+    var endTime2;
+    var total2;
+    var difference2;
+    var timer2;
+    element = document.getElementById('myImage2')
+    if (element.src.match("On")){
+        endTime2 = new Date();
+        timer2 = endTime2.getHours() + ":" + endTime2.getMinutes() + ":" + endTime2.getSeconds();
+        difference2 = Math.round(endTime2 - startTime2);
+        total2 = Math.round((difference2 / 60000) / 60);
+        total2 = total2 * 52;
+        element.src = "/Proyecto primer parcial/images/Off.png"
+        document.getElementById('fin2').innerHTML = "Hora del fin: " + timer2;
+        document.getElementById('total2').innerHTML = "Total: " + total2;
+    } else {
+        startTime2 = new Date();
+        document.getElementById('fin2').innerHTML = "Hora del fin: ";
+        document.getElementById('total2').innerHTML = "Total: ";
+        timer2 = startTime2.getHours() + ":" + startTime2.getMinutes() + ":" + startTime2.getSeconds();
+        element.src = "/Proyecto primer parcial/images/On.png"
+        document.getElementById('inicio2').innerHTML = "Hora de Inicio: " + timer2;
+    }   
+}
+const  changeImage3 = () => {
+    var startTime3 = new Date();
+    var endTime3;
+    var total3;
+    var difference3;
+    var timer3;
+    element = document.getElementById('myImage3')
+    if (element.src.match("On")){
+        endTime3 = new Date();
+        timer3 = endTime3.getHours() + ":" + endTime3.getMinutes() + ":" + endTime3.getSeconds();
+        difference3 = Math.round(endTime3 - startTime3);
+        total3 = Math.round((difference3 / 60000) / 60);
+        total3 = total3 * 52;
+        element.src = "/Proyecto primer parcial/images/Off.png"
+        document.getElementById('fin2').innerHTML = "Hora del fin: " + timer2;
+        document.getElementById('total2').innerHTML = "Total: " + total2;
+    } else {
+        startTime2 = new Date();
+        document.getElementById('fin2').innerHTML = "Hora del fin: ";
+        document.getElementById('total2').innerHTML = "Total: ";
+        timer2 = startTime2.getHours() + ":" + startTime2.getMinutes() + ":" + startTime2.getSeconds();
+        element.src = "/Proyecto primer parcial/images/On.png"
+        document.getElementById('inicio2').innerHTML = "Hora de Inicio: " + timer2;
+    }   
+}
+const  changeImage2 = () => {
+    var startTime2 = new Date();
+    var endTime2;
+    var total2;
+    var difference2;
+    var timer2;
+    element = document.getElementById('myImage2')
+    if (element.src.match("On")){
+        endTime2 = new Date();
+        timer2 = endTime2.getHours() + ":" + endTime2.getMinutes() + ":" + endTime2.getSeconds();
+        difference2 = Math.round(endTime2 - startTime2);
+        total2 = Math.round((difference2 / 60000) / 60);
+        total2 = total2 * 52;
+        element.src = "/Proyecto primer parcial/images/Off.png"
+        document.getElementById('fin2').innerHTML = "Hora del fin: " + timer2;
+        document.getElementById('total2').innerHTML = "Total: " + total2;
+    } else {
+        startTime2 = new Date();
+        document.getElementById('fin2').innerHTML = "Hora del fin: ";
+        document.getElementById('total2').innerHTML = "Total: ";
+        timer2 = startTime2.getHours() + ":" + startTime2.getMinutes() + ":" + startTime2.getSeconds();
+        element.src = "/Proyecto primer parcial/images/On.png"
+        document.getElementById('inicio2').innerHTML = "Hora de Inicio: " + timer2;
+    }   
+}
+const  changeImage2 = () => {
+    var startTime2 = new Date();
+    var endTime2;
+    var total2;
+    var difference2;
+    var timer2;
+    element = document.getElementById('myImage2')
+    if (element.src.match("On")){
+        endTime2 = new Date();
+        timer2 = endTime2.getHours() + ":" + endTime2.getMinutes() + ":" + endTime2.getSeconds();
+        difference2 = Math.round(endTime2 - startTime2);
+        total2 = Math.round((difference2 / 60000) / 60);
+        total2 = total2 * 52;
+        element.src = "/Proyecto primer parcial/images/Off.png"
+        document.getElementById('fin2').innerHTML = "Hora del fin: " + timer2;
+        document.getElementById('total2').innerHTML = "Total: " + total2;
+    } else {
+        startTime2 = new Date();
+        document.getElementById('fin2').innerHTML = "Hora del fin: ";
+        document.getElementById('total2').innerHTML = "Total: ";
+        timer2 = startTime2.getHours() + ":" + startTime2.getMinutes() + ":" + startTime2.getSeconds();
+        element.src = "/Proyecto primer parcial/images/On.png"
+        document.getElementById('inicio2').innerHTML = "Hora de Inicio: " + timer2;
+    }   
+}
+const  changeImage2 = () => {
+    var startTime2 = new Date();
+    var endTime2;
+    var total2;
+    var difference2;
+    var timer2;
+    element = document.getElementById('myImage2')
+    if (element.src.match("On")){
+        endTime2 = new Date();
+        timer2 = endTime2.getHours() + ":" + endTime2.getMinutes() + ":" + endTime2.getSeconds();
+        difference2 = Math.round(endTime2 - startTime2);
+        total2 = Math.round((difference2 / 60000) / 60);
+        total2 = total2 * 52;
+        element.src = "/Proyecto primer parcial/images/Off.png"
+        document.getElementById('fin2').innerHTML = "Hora del fin: " + timer2;
+        document.getElementById('total2').innerHTML = "Total: " + total2;
+    } else {
+        startTime2 = new Date();
+        document.getElementById('fin2').innerHTML = "Hora del fin: ";
+        document.getElementById('total2').innerHTML = "Total: ";
+        timer2 = startTime2.getHours() + ":" + startTime2.getMinutes() + ":" + startTime2.getSeconds();
+        element.src = "/Proyecto primer parcial/images/On.png"
+        document.getElementById('inicio2').innerHTML = "Hora de Inicio: " + timer2;
+    }   
+}
